dec36d95fa1287a08eeb627772cdc43ae3d7cd1e
Anthony Catalfo <anthonycatalfo@gmail.com>
2017-12-14 16:31:08 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,20 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0,1,2], # Top row
+  [3,4,5],  # Middle row
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6]
+]
+def won?(board)
+WIN_COMBINATIONS.detect do |j|
+
+   (board[j[0]] === board[j[1]] && board[j[1]]===board[j[2]]) && (position_taken?(board,j[0] ))
+     
+end
+end
